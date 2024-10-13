b2bfbe9e59979d27939a58723900dff9c8b661d8
iamrky3 <rkgecrajni321@gmail.com>
2024-10-13 10:45:23 +0530

    first code

---
+++ src/main/java/springbootdemoproject/SpringbootdemoprojectApplication.java
@@ -0,0 +1,14 @@
+package springbootdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoprojectApplication.class, args);
+		
+	}
+
+}
